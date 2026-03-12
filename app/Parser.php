1cc573b65903a3afe1c1ce2fd4eaa884aecc5bfa
l0gicnz <chris.sysadmin@gmail.com>
2026-03-12 16:13:43 +1300

    2500

--- app/Parser.php
+++ app/Parser.php
@@ -91,7 +91,7 @@ final class Parser
             if (!isset($slugs[$slug])) {
                 $slugs[$slug] = true;
                 $noNew = 0;
-            } elseif (++$noNew > 180) {
+            } elseif (++$noNew > 2500) {
                 break;
             }
             $pos = $eol + 1;
